6549edb981497e7336e42990ac16227e33f8adfd
belek <belek@maddevs.io>
2025-01-30 17:46:12 +0600

    Added python 3.12, fixed signature, bumped version.

--- django_grpc/signals/wrapper.py
+++ django_grpc/signals/wrapper.py
@@ -1,4 +1,6 @@
 from functools import wraps
+from typing import Dict
+
 import grpc
 from grpc._utilities import RpcMethodHandler
 
@@ -41,7 +43,11 @@ class SignalWrapper:
 
         return RpcMethodHandler(**kwargs)
 
-    def add_registered_method_handlers(self, service_name: str, registered_method_handlers: tuple):
+    def add_registered_method_handlers(
+        self,
+        service_name: str,
+        method_handlers: Dict[str, grpc.RpcMethodHandler],
+    ):
         # do nothing; see https://github.com/grpc/grpc/issues/36683
         # and https://github.com/grpc/grpc/pull/36696
         pass
